25eae14d1364b48afd9a88f322bee40fee93b761
rullyfadheli <rullyfadheli@gmail.com>
2025-01-06 20:58:46 +0700

    enhance registration form with password confirmation and login link

--- app/components/RegisterForm.js
+++ app/components/RegisterForm.js
@@ -1,14 +1,47 @@
-import Button from "./Button";
+import Link from "next/link";
 
 function RegisterForm() {
   return (
-    <form className="flex flex-col my-2">
-      <input className="h-4" type="text" id="username" name="username" />
+    <div>
+      {" "}
+      <form className="flex flex-col my-2">
+        <input
+          className="my-2 h-12 rounded-md p-2"
+          type="text"
+          id="username"
+          name="username"
+          placeholder="Username"
+        />
 
-      <input type="password" id="password" name="password" />
+        <input
+          className="my-2 h-12 rounded-md p-2"
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Password"
+        />
 
-      <button className="bg-primary max-w-[50%]">Register</button>
-    </form>
+        <input
+          className="my-2 h-12 rounded-md p-2"
+          type="password"
+          id="confirm-password"
+          name="confirm-password"
+          placeholder="Confirm password"
+        />
+
+        <div className="flex justify-center mt-2 h-12">
+          <button className="bg-primary text-secondary w-full max-w-[50%] rounded-md">
+            Register
+          </button>
+        </div>
+      </form>
+      <div className="flex justify-center mt-2">
+        <span>Already have an account?</span>
+        <Link className="mx-2 font-bold text-primary" href="/login">
+          Login
+        </Link>
+      </div>
+    </div>
   );
 }
 
--- app/register/page.js
+++ app/register/page.js
@@ -2,8 +2,8 @@ import RegisterForm from "../components/RegisterForm";
 
 function RegisterPage() {
   return (
-    <div className="h-screen md:justify-center md:flex bg-[#A9411D] w-full ">
-      <div className="h-full font-inter w-full max-w-3xl bg-[#EBE5DD] p-6 flex-col centerElement">
+    <div className="h-screen flex items-center justify-center bg-[#A9411D] w-full ">
+      <div className="font-inter h-96 w-full max-w-2xl bg-[#EBE5DD] p-6 flex-col centerElement rounded-md">
         <h1 className="text-center">Register</h1>
         <RegisterForm />
       </div>
