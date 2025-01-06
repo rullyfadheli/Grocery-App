7af12b8534f8801ff206336477c3296649e3bf60
rullyfadheli <rullyfadheli@gmail.com>
2025-01-06 07:12:42 +0700

    Starting the project

---
+++ app/home/page.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="md:justify-center md:flex bg-[#A9411D]">
+      <div className="font-inter min-h-full w-full max-w-3xl bg-[#EBE5DD] p-6">
+        Hello World
+      </div>
+    </div>
+  );
+}
