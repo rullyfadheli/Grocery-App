216dca693a20852e3d5c6398a7714038dff81687
rullyfadheli <rullyfadheli@gmail.com>
2025-01-07 06:40:07 +0700

    add database configuration and user model files

---
+++ backend/config/database.js
@@ -0,0 +1,11 @@
+const dbPool = require("mysql2");
+require("dotenv").config();
+
+dbPool.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+});
+
+module.exports = dbPool.promise();
