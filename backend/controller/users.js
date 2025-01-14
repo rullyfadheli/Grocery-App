1c98013b58eb61fa034d05eac2bb3706e7bb7b95
rullyfadheli <rullyfadheli@gmail.com>
2025-01-14 17:58:31 +0700

    Refactor user registration and login functions with detailed JSDoc comments; clean up database configuration by removing console log

--- backend/controller/users.js
+++ backend/controller/users.js
@@ -3,6 +3,13 @@ const userModels = require("../models/users");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+/**
+ * Registers a user with the provided email, password, username, and confirmPassword.
+ *
+ * @param {Object} request - The request object containing the user's information.
+ * @param {Object} response - The response object to send the registration status.
+ * @returns {undefined}
+ */
 async function register(request, response) {
   const { email, password, username, confirmPassword } = request.body;
   console.log(request.body);
@@ -30,7 +37,16 @@ async function register(request, response) {
   }
 }
 
-async function login(request, response) {
+/**
+ * Authenticates the user by comparing the provided password with the hashed password stored in the database.
+ * If the passwords match, generates access and refresh tokens and updates the user's refresh token in the database.
+ * Sets the refresh token as a cookie and returns the access token in the response.
+ * If the password is incorrect or the user's email is not found, returns an appropriate error message.
+ *
+ * @param {Object} request - The request object.
+ * @param {Object} response - The response object.
+ * @returns {Object} - The response object containing the access token or an error message.
+ */ async function login(request, response) {
   const { email, password } = request.body;
   const user = await userModels.getUserByEmail(email);
   const match = await bcrypt.compare(password, user[0][0].password);
--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -1,20 +1,18 @@
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
 const SECRET_KEY = process.env.ACCESS_TOKEN_SECRET;
+require("dotenv").config();
 
-function verifyToken(request, response, next) {
-  const authHeader = request.headers["authorization"];
+function verifyToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
   const token = authHeader.split("Bearer ")[1];
-  console.log(token);
-  console.log(authHeader);
+
   try {
-    const token = jwt.verify(token, SECRET_KEY);
-    request.user = token;
+    const decoded = jwt.verify(token, SECRET_KEY);
+    req.user = decoded;
+    console.log(decoded);
     next();
   } catch (error) {
-    return response.status(403).json({
-      message: "Invalid token",
-    });
+    return res.status(403).json({ message: "Invalid token" });
   }
 }
 
