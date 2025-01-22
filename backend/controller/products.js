f9cc2c41a4b295560c8788f0c5f866d3205001f7
rullyfadheli <rullyfadheli@gmail.com>
2025-01-22 22:15:50 +0700

    Add JSDoc comments for product controller functions and enhance route documentation

--- backend/controller/products.js
+++ backend/controller/products.js
@@ -1,5 +1,11 @@
 const productsModel = require("../models/products");
-
+/**
+ * Asynchronously retrieves all products.
+ *
+ * @param {Object} request - The request object.
+ * @param {Object} response - The response object.
+ * @returns {undefined}
+ */
 async function getAllProducts(request, response) {
   try {
     const data = await productsModel.getAllProducts();
@@ -7,6 +13,13 @@ async function getAllProducts(request, response) {
   } catch (error) {}
 }
 
+/**
+ * Function to add a new product.
+ *
+ * @param {Object} request - The request object.
+ * @param {Object} response - The response object.
+ * @returns {Object} - The response object.
+ */
 async function addProduct(request, response) {
   const { name, price, description, category } = request.body;
 
@@ -31,7 +44,13 @@ async function addProduct(request, response) {
   }
 }
 
-async function getProductByName(request, response) {
+/**
+ * Retrieves a product by its name from the database and sends it as a JSON response.
+ *
+ * @param {string} name - The name of the product.
+ * @returns {void}
+ * @throws {Error} If there is an error retrieving the product or sending the response.
+ */ async function getProductByName(request, response) {
   const { name } = request.body;
 
   try {
