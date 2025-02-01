37e1c0f97c1b8b0e6618ce88b2788490cff24d9b
rullyfadheli <rullyfadheli@gmail.com>
2025-02-01 19:04:48 +0700

    remove unused components and styles from the backend

--- backend/app/components/Button.js
+++
@@ -1,9 +0,0 @@
-function Button({ text, onClick, type, style }) {
-  return (
-    <button type={type} onClick={onClick} className={style}>
-      {text}
-    </button>
-  );
-}
-
-export default Button;
--- backend/app/page.js
+++
@@ -1,5 +0,0 @@
-import Home from "./home/page";
-//test
-export default function Main() {
-  return <Home />;
-}
