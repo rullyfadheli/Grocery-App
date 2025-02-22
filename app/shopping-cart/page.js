b45c0753328da032d7f374d970405fd2703e5204
rullyfadheli <rullyfadheli@gmail.com>
2025-02-22 15:30:30 +0700

    remove Button component; add ShoppingCart page with TopNavigation

---
+++ app/shopping-cart/page.js
@@ -0,0 +1,12 @@
+import TopNavigation from "../components/TopNavigation";
+import Image from "next/image";
+function ShoppingCart() {
+  return (
+    <div className="">
+      <TopNavigation />
+      <p>Shopping cart</p>
+    </div>
+  );
+}
+
+export default ShoppingCart;
