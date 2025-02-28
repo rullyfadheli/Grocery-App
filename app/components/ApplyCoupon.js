eca87b23342283e7f25ee76ece02a8cbdfacba78
rullyfadheli <rullyfadheli@gmail.com>
2025-02-28 07:08:31 +0700

    update ApplyCoupon component; adjust icon sizes for improved visibility

--- app/components/ApplyCoupon.js
+++ app/components/ApplyCoupon.js
@@ -3,14 +3,14 @@ function ApplyCoupon() {
   return (
     <div className="w-full h-20 flex justify-between font-inter">
       <span>
-        <Image src={"/percent.png"} width={15} height={15} alt="Percent icon" />
+        <Image src={"/percent.png"} width={30} height={30} alt="Percent icon" />
       </span>
       <p>APPLY COUPON</p>
       <span>
         <Image
           src={"/Arrow Right.png"}
-          width={15}
-          height={15}
+          width={13}
+          height={13}
           alt="Arrow Right "
         />
       </span>
