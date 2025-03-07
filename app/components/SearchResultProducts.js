125c77c03b6533c1c612749d96dd1802be76c7d2
rullyfadheli <rullyfadheli@gmail.com>
2025-03-07 17:25:29 +0700

    implement search functionality in SearchResultProducts component; add loading state and handle query results with error messaging

--- app/components/SearchResultProducts.js
+++ app/components/SearchResultProducts.js
@@ -1,17 +1,100 @@
+"use client";
 import { SearchProductContext } from "../context/AppContext";
+import { useContext, useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
-import { useContext } from "react";
-function SearchProductResult() {
-  const { searchResult } = useContext(SearchProductContext);
-  console.log(searchResult);
+import LoadingAnimation from "./LoadingAnimation";
+import NotFoundMessage from "./elements/NotFoundMessage";
+
+function SearchProductResult({ searchQuery }) {
+  // State to store fetched data from SearchBar.js at page product-category
+  const { searchResult, setSearchResult } = useContext(SearchProductContext);
+  console.log("searchQuery:", searchQuery);
+
+  // State to store fetched data from HomeSearchBar.js at page home
+  const [querySearchResult, setQuerySearchResult] = useState(null);
+  console.log(querySearchResult);
+  // State to manage loading status
+  const [loading, setLoading] = useState(false);
+
+  // Function to get search query from home search bar
+  // and fetch the data from server and set the searchResult state
+  useEffect(() => {
+    console.log();
+
+    // Run the function if searchQuery is not null
+    if (searchQuery && searchQuery !== "") {
+      async function searchProductByName() {
+        try {
+          setLoading(true); // Set loading to true while fetching
+          const response = await fetch(
+            "https://grocery-app.my.id/api/search-product",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ name: searchQuery }),
+            }
+          );
+          const data = await response.json();
+          setQuerySearchResult(data);
+          setLoading(false); // Set loading to false when fetching is complete
+        } catch (error) {
+          console.log(error);
+          setLoading(false); // Set loading to false on error
+        }
+      }
+
+      searchProductByName();
+    } else {
+      console.log("searchQuery is null or empty, skipping fetch");
+    }
+
+    return () => {
+      // Cleanup
+    };
+  }, [searchQuery]);
+
   return (
-    <div className="w-3/4">
-      <div className="gap-4 flex flex-wrap p-2 w-full ">
-        {searchResult &&
-          searchResult.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </div>
+    <div className="w-3/4 font-inter">
+      {/* Show loading animation when loading */}
+      {loading ? (
+        <div className="flex justify-center items-center h-96">
+          <LoadingAnimation />
+        </div>
+      ) : (
+        // Display products or message after loading
+        <>
+          {searchResult &&
+          (!querySearchResult || !Array.isArray(querySearchResult)) ? (
+            <div
+              className={`gap-4 flex-wrap p-2 w-full ${
+                searchResult ? "flex " : "hidden"
+              }`}
+            >
+              {Array.isArray(searchResult) ? (
+                searchResult.map((product) => (
+                  <ProductCard key={product.id} product={product} />
+                ))
+              ) : (
+                <NotFoundMessage data={searchResult?.message} />
+              )}
+            </div>
+          ) : querySearchResult && Array.isArray(querySearchResult) ? (
+            <div
+              className={`gap-4 flex-wrap p-2 w-full ${
+                querySearchResult ? "flex " : "hidden"
+              }`}
+            >
+              {querySearchResult.map((product) => (
+                <ProductCard key={product.id} product={product} />
+              ))}
+            </div>
+          ) : (
+            <NotFoundMessage data={querySearchResult?.message} />
+          )}
+        </>
+      )}
     </div>
   );
 }
