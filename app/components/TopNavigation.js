1016d36b2d11c55dece645a9fd56d762acecc52e
rullyfadheli <rullyfadheli@gmail.com>
2025-03-07 17:25:39 +0700

    refactor TopNavigation component; simplify props and clean up unused variables

--- app/components/TopNavigation.js
+++ app/components/TopNavigation.js
@@ -2,18 +2,9 @@
 import Link from "next/link";
 import Button from "./elements/Button";
 
-function TopNavigation({
-  contentLeft,
-  contentRight,
-  title,
-  text,
-  onClick,
-  type,
-  style,
-  href,
-}) {
+function TopNavigation({ contentLeft, contentRight, title, onClick }) {
   return (
-    <div className="flex justify-between items-center p-4 bg-transparrent shadow text-black py-5 font-inter">
+    <div className="flex justify-between items-center p-4 bg-transparrent text-black py-5 font-inter">
       <Link href={"/"}>{contentLeft}</Link>
       <span className="font-bold">{title}</span>
       <Button text={contentRight} onClick={onClick} />
