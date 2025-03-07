c6066a4abca2c53f969568f3433e821fc74b248e
rullyfadheli <rullyfadheli@gmail.com>
2025-03-07 17:24:11 +0700

    refactor CategorySidebar component; update styling and layout for improved user experience

--- app/components/CategorySidebar.js
+++ app/components/CategorySidebar.js
@@ -16,14 +16,15 @@ const CategorySidebar = () => {
   const router = useRouter();
 
   return (
-    <div className="w-1/4 md:max-w-[20%] bg-secondary flex justify-center relative">
-      <div className="bg-secondary py-4 h-content flex-col justify-items-center">
+    <div className="w-1/4 md:max-w-[20%] bg-tertiary flex justify-center relative">
+      <div className="bg-tertiary w-full flex-col justify-items-center">
         {data.map((items, index) => (
           <div
             key={index}
-            className="h-content w-20"
+            className="h-content w-full"
             onClick={() => {
               setActive(items.name);
+              params.delete("search");
               params.set("category", "");
               params.set("category", items.name);
               setToggleSearchResult(false);
@@ -31,16 +32,34 @@ const CategorySidebar = () => {
               router.push(`http://localhost:3000/product-category?${params}`);
             }}
           >
-            <div
-              className={`w-20 h-20 my-4 rounded-lg ${
-                active === items.name
-                  ? "bg-primary transition-colors delay-100 duration-300"
-                  : "bg-tertiary"
-              } flex justify-center items-center`}
-            >
-              <Image src={items.img} width={30} height={30} alt={items.alt} />
+            <div className="flex justify-between w-full h-full my-2">
+              <div></div>
+              <div className="flex flex-col items-center">
+                <div
+                  className={`w-20 h-20 my-4 rounded-lg ${
+                    active === items.name
+                      ? "bg-primary transition-colors delay-100 duration-300"
+                      : "bg-tertiary"
+                  } flex justify-center items-center`}
+                >
+                  <Image
+                    src={items.img}
+                    width={40}
+                    height={40}
+                    alt={items.alt}
+                    style={{ width: "30px", height: "auto" }}
+                  />
+                </div>
+                <p className="text-center leading-1">{items.name}</p>
+              </div>
+              <div
+                className={`w-1 h-24 mt-3 rounded-sm ${
+                  active === items.name
+                    ? "bg-primary"
+                    : "bg-tertiary transition-colors duration-100"
+                }`}
+              ></div>
             </div>
-            <p className="text-center">{items.name}</p>
           </div>
         ))}
       </div>
