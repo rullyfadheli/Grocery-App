db98f36775abfe35d755513da225d644a2db21f2
rullyfadheli <rullyfadheli@gmail.com>
2025-03-07 17:25:55 +0700

    refactor ProductCategory component; enhance search functionality with dynamic loading and improved search query handling

--- app/Product-category/page.js
+++ app/Product-category/page.js
@@ -1,30 +1,37 @@
 "use client";
 import { useSearchParams } from "next/navigation";
-import { useState, useContext } from "react";
+import { Suspense, useContext, useState } from "react";
+import { SearchProductContext } from "../context/AppContext";
 import CategorySidebar from "../components/CategorySidebar";
 import ProductList from "../components/ProductList";
 import TopNavigation from "../components/TopNavigation";
-import SearchBar from "../components/SearchBar";
-import { SearchProductContext } from "../context/AppContext";
+import dynamic from "next/dynamic";
 import Image from "next/image";
 import SearchProductResult from "../components/SearchResultProducts";
+import SearchBar from "../components/SearchBar";
 
 export default function ProductCategory() {
+  const searchParams = useSearchParams();
+
   // Getting state from context
   const {
     searchClick,
     setSearchClick,
-    searchResult,
-    setSearchResult,
     toggleSearchResult,
     setToggleSearchResult,
   } = useContext(SearchProductContext);
 
-  // console.log(searchRequestData);
+  // console.log(toggleSearchResult);
+  //Getting params from URL to display product by category
   const params = useSearchParams().get("category");
   const decoded = decodeURIComponent(params);
 
-  // Function to show search input by clicking search icon
+  console.log(decoded);
+
+  const searchQuery = useSearchParams().get("search");
+  const [decodedSearch] = useState(decodeURIComponent(searchQuery));
+
+  // Function to show search input by clicking the search icon
   function handleToggleSearch() {
     setSearchClick(!searchClick);
     setToggleSearchResult(!toggleSearchResult);
@@ -33,8 +40,10 @@ export default function ProductCategory() {
     <div className="bg-primary w-full flex justify-center">
       <div className="flex flex-col bg-tertiary w-full max-w-[1080px]">
         {searchClick ? (
+          // Search bar that could be toggled when the user click the search icon
           <SearchBar />
         ) : (
+          // Navigation bar that includes search button, title, and back button
           <TopNavigation
             onClick={handleToggleSearch}
             contentLeft={
@@ -56,12 +65,18 @@ export default function ProductCategory() {
             }
           />
         )}
+
         <div className="flex">
           <CategorySidebar />
-          {toggleSearchResult ? (
-            <SearchProductResult />
+          {toggleSearchResult || searchParams.has("search") ? (
+            //Component that shows the products from search
+            <SearchProductResult searchQuery={decodedSearch} />
           ) : (
-            <ProductList productCategory={decoded} />
+            //Component that shows products from sidebar category
+            //This component receives params from url ?category=decoded
+            <Suspense>
+              <ProductList productCategory={decoded} />
+            </Suspense>
           )}
         </div>
       </div>
